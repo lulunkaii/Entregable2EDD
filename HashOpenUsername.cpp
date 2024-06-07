e8a1f645ab06085570dfeeeced0d7a4a60f5226f
ElPerroGuaton <114620518+ElPerroGuaton@users.noreply.github.com>
2024-06-07 00:03:43 -0400

    Add files via upload

--- HashOpenUsername.cpp
+++ HashOpenUsername.cpp
@@ -4,108 +4,80 @@
 #include "HashOpenUsername.h"
 #include <cmath>
 
-
 using namespace std;
 
-HashOpenUsername::HashOpenUsername(){
-
+// Constructor de la clase HashOpenUsername.
+HashOpenUsername::HashOpenUsername() {
+    // Inicializa la tabla hash.
     tabla = new list<UserInfo>[hashCapacity];
-
-    for(int i=0; i< hashCapacity; i++){
-
-        tabla[i].clear();
-
-    }
-
-    
 }
 
-
-void HashOpenUsername::put(string key, UserInfo infoUser){
-
-    int claveHash = hashFunction(key);
-
-    currentSize++;
-
-    infoUser.key = key;
-
-    tabla[claveHash].push_back(infoUser);
-
+// Método para insertar un elemento en la tabla hash.
+void HashOpenUsername::put(string key, UserInfo infoUser) {
+    int claveHash = hashFunction(key); // Calcula el índice hash para la clave.
+    currentSize++;                     // Incrementa el tamaño actual de la tabla.
+    tabla[claveHash].push_back(infoUser); // Inserta el elemento en la lista correspondiente al índice hash.
 }
 
-void HashOpenUsername::remove(string key){
-
-    int claveHash = hashFunction(key);
-    auto& cell = tabla[claveHash];
-    auto Iterador = begin(cell);
+// Método para eliminar un elemento de la tabla hash.
+void HashOpenUsername::remove(string key) {
+    int claveHash = hashFunction(key); // Calcula el índice hash para la clave.
+    auto& cell = tabla[claveHash];     // Obtiene la lista correspondiente al índice hash.
+    auto Iterador = begin(cell);       // Inicializa el iterador para recorrer la lista.
     bool llaveExiste = false;
 
-    for(; Iterador != end(cell);Iterador++){
-
-        if(Iterador->key == key){
-
+    // Recorre la lista buscando el elemento con la clave especificada.
+    for (; Iterador != end(cell); Iterador++) {
+        if (Iterador->user_name == key) { // Compara con el nombre de usuario.
             llaveExiste = true;
-            Iterador = cell.erase(Iterador);
-            currentSize--;
+            Iterador = cell.erase(Iterador); // Elimina el elemento de la lista.
+            currentSize--;                   // Decrementa el tamaño actual de la tabla.
             break;
         }
     }
 }
 
+// Método para buscar un elemento en la tabla hash.
+bool HashOpenUsername::search(string key) {
+    int claveHash = hashFunction(key); // Calcula el índice hash para la clave.
+    int i = 0;  // Contador por si no hay ningun elemento en la tabla
 
-bool HashOpenUsername::search(string key){
-
-    int claveHash = hashFunction(key);
-    int i=0;
-
-    while(tabla[claveHash].empty() == false && i<hashCapacity){
-
-        for(const auto& IdInfo : tabla[claveHash] ){
-        
-            if (IdInfo.key == key) {
-
+    // Mientras haya elementos en la lista correspondiente y no se haya recorrido toda la capacidad de la tabla.
+    while (tabla[claveHash].empty() == false && i < hashCapacity) {
+        // Recorre la lista buscando el elemento con la clave especificada.
+        for (const auto& IdInfo : tabla[claveHash]) {
+            if (IdInfo.user_name == key) { // Compara con el nombre de usuario.
                 return true;
             }
-
         }
-
         i++;
         claveHash++;
-        claveHash %= hashCapacity;
-
+        claveHash %= hashCapacity; // Asegura que el índice hash esté dentro de los límites de la tabla.
     }
-
     return false;
-
 }
 
-
-int HashOpenUsername::hashFunction(string key){
-
+// Función hash para calcular el índice en la tabla hash.
+int HashOpenUsername::hashFunction(string key) {
     int sum = 0;
 
+    // Suma los valores ASCII de los caracteres en la clave.
     for (char c : key) {
         sum += static_cast<int>(c);
     }
 
-    sum = (sum*113)%hashCapacity;
+    // Multiplica la suma por 113 y aplica el operador módulo con la capacidad de la tabla.
+    sum = (sum * 113) % hashCapacity;
 
     return sum;
-
 }
 
-int HashOpenUsername::size(){
-
+// Método para obtener el tamaño actual de la tabla hash.
+int HashOpenUsername::size() {
     return currentSize;
-
 }
 
-bool HashOpenUsername::isEmpty(){
-
-    if(currentSize == 0){
-        return true;
-    }
-
-    else return false;
-
+// Método para verificar si la tabla hash está vacía.
+bool HashOpenUsername::isEmpty() {
+    return currentSize == 0;
 }
